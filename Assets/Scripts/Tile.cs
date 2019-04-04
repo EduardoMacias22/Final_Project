9003a088294893c76289a0aad574156ce937347b
student <>
2019-04-03 20:40:19 -0500

    Adding Sprite Unity

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{ 
+    public int X, Y;
+
+    public Transform ConnectPoint;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
